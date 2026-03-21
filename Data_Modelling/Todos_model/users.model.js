52c12506a594b57a895405065caaa43d7b5aa198
Akhilesh Aher <aherakhilesh88@gmail.com>
2026-03-21 15:21:56 +0530

    Typical Todos Models

---
+++ Data_Modelling/Todos_model/users.model.js
@@ -0,0 +1,22 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is Required"],
+    },
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema);

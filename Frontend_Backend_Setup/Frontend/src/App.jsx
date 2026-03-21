1c55c7d0899d386d85517eff73c5e5342955dbe1
Akhilesh Aher <aherakhilesh88@gmail.com>
2026-03-21 14:58:27 +0530

    Frontend connection axios done

--- Frontend_Backend_Setup/Frontend/src/App.jsx
+++ Frontend_Backend_Setup/Frontend/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { useState } from "react";
+import axios from 'axios';
+import { useEffect } from "react";
+
 
 function App() {
+
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data)
+      }).catch((error) => {
+        console.log(error);
+      });
+  });
+
   return (
-    <div>
-      ROOT PAgE
-    </div>
+    <>
+      <h1>You are on Root Page</h1>
+      <p>JOKES : {jokes.length}</p>
+
+      {
+        jokes.map((joke, index) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+    </>
   )
 }
 
-export default App
+export default App;

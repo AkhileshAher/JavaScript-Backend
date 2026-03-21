7d4e6c062160b452b6005b3410bc6340e251fe90
Akhilesh Aher <aherakhilesh88@gmail.com>
2026-03-21 14:54:26 +0530

    Server File Final Setup

--- Frontend_Backend_Setup/Backend/server.js
+++ Frontend_Backend_Setup/Backend/server.js
@@ -1,7 +1,55 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
 
 const app = express();
 
-app.listen(process.env.PORT ,() => {
-    console.log(`Server is Listening to PORT : ${process.env.PORT}`);
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+
+app.get("/", (req, res) => {
+  res.send("Server is Ready and Serving");
+});
+
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A Joke",
+      content: "This is a Joke",
+    },
+    {
+      id: 2,
+      title: "Another Joke",
+      content: "This is a Another Joke",
+    },
+    {
+      id: 3,
+      title: "A Third Joke",
+      content: "This is Third a Joke",
+    },
+    {
+      id: 4,
+      title: "A Fourth Joke",
+      content: "This is a Fourth Joke",
+    },
+    {
+      id: 5,
+      title: "A 5 Joke",
+      content: "This is a Fifth Joke",
+    },
+    {
+      id: 6,
+      title: "A  6 Joke",
+      content: "This is a Sixth Joke",
+    },
+  ];
+
+  res.send(jokes);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Listening on Port  : ${process.env.PORT}`);
 });
